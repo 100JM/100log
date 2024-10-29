a4c67d005a784dc06c8e873cf6626a82ae97dec0
100JM <bjm_9310@naver.com>
2024-10-29 18:00:06 +0900

    update

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/app/components/Header';
 import Contact from '@/app/components/Contact';
 import Tags from '@/app/components/Tags';
+import { Skeleton } from '@/components/ui/skeleton';
 
 import { getPostBySlug } from '@/app/utils/mdx';
 import { MDXRemote } from 'next-mdx-remote/rsc';
@@ -22,14 +23,17 @@ export default async function PostPage({
             <Header />
             <main className="w-full h-full max-w-[900px] m-auto flex justify-center items-center flex-col break-keep break-words">
                 <article className="w-full my-10 p-6 flex justify-center">
-                    <div className="w-full">
-                        <h1 className="text-6xl font-bold mb-4">{post.title}</h1>
+                    <div className="w-3/4">
+                        <h1 className="text-5xl mb-4">{post.title}</h1>
                         <div className="flex flex-wrap text-lg mb-8 justify-start items-end text-slate-500 gap-6">                            
                             <Tags tags={post.tags} date={post.date} />
                         </div>
                         <div className="prose prose-lg">
                             <MDXRemote source={post.content} />
                         </div>
+                    </div>
+                    <div className="w-1/4">
+                        <Skeleton className="h-[450px] rounded-xl" />
                     </div>
                 </article>
                 <Contact />
