947a21b8553e27e9b1b8a7c426d94f24e5539bc2
100JM <bjm_9310@naver.com>
2024-10-31 17:29:06 +0900

    기초 CSS 설정

--- app/components/PostList.tsx
+++ app/components/PostList.tsx
@@ -1,8 +1,13 @@
-'use client';
+// 'use client';
+
+import { getAllPosts } from "../utils/mdx";
 
 import { Skeleton } from "@/components/ui/skeleton"
 
-const PostList: React.FC = () => {
+const PostList = async () => {
+    const post = await getAllPosts();
+
+    console.log(post);
     return (
         <div className="w-[90%] grid gap-6 gap-y-10 mb-6 post-list">
             {Array.from({length: 10}, (_, index) => {
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -7,11 +7,7 @@ import { getPostBySlug } from '@/app/utils/mdx';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { notFound } from 'next/navigation';
 
-export default async function PostPage({
-    params,
-}: {
-    params: { slug: string };
-}) {
+const PostPage = async ({ params, }: { params: { slug: string }; }) => {
     const post = await getPostBySlug(params.slug);
 
     if (!post) {
@@ -21,21 +17,23 @@ export default async function PostPage({
     return (
         <div>
             <Header />
-            <main className="w-full h-full max-w-[900px] m-auto flex justify-center items-center flex-col break-keep break-words">
-                <article className="w-full my-10 p-6 flex justify-center">
-                    <div className="w-3/4">
-                        <h1 className="text-5xl mb-4">{post.title}</h1>
-                        <div className="flex flex-wrap text-lg mb-8 justify-start items-end text-slate-500 gap-6">                            
-                            <Tags tags={post.tags} date={post.date} />
+            <main className="w-full h-full max-w-[1100px] m-auto flex justify-center items-center flex-col break-keep break-words">
+                <div className="w-full flex justify-center">
+                    <article className="w-full my-10 p-6 flex justify-center">
+                        <div className="w-full">
+                            <h1 className="text-5xl mb-4">{post.title}</h1>
+                            <div className="flex flex-wrap text-lg mb-8 justify-start items-end text-slate-500 gap-6">
+                                <Tags tags={post.tags} date={post.date} />
+                            </div>
+                            <div className="post-contents">
+                                <MDXRemote source={post.content} />
+                            </div>
                         </div>
-                        <div className="prose prose-lg">
-                            <MDXRemote source={post.content} />
-                        </div>
+                    </article>
+                    <div className="w-[30%] hidden xxl:block">
+                        <Skeleton className="h-[450px] rounded-xl mt-16 sticky top-28" />
                     </div>
-                    <div className="w-1/4">
-                        <Skeleton className="h-[450px] rounded-xl" />
-                    </div>
-                </article>
+                </div>
                 <Contact />
             </main>
             <footer className="flex justify-center items-center w-full h-20 text-sm font-thin text-slate-400 dark:text-[#f7f9fb]">
@@ -44,3 +42,5 @@ export default async function PostPage({
         </div>
     );
 };
+
+export default PostPage;
