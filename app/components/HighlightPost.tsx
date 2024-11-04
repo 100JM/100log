dbf2d7081a121bd36d839383e7be5e7d6f9ec0d2
100JM <bjm_9310@naver.com>
2024-11-04 16:24:32 +0900

    css update

--- app/components/HighlightPost.tsx
+++ app/components/HighlightPost.tsx
@@ -16,14 +16,13 @@ const HighlightPost: React.FC = async () => {
                 href={`posts/${latestPost.slug}`}
                 className="h-[600px] w-full cursor-pointer hover:text-slate-500"
             >
-                
                     <div className="text-4xl flex items-center justify-start py-2 h-[70px]">
                         <i className="ri-pushpin-2-line"></i>
                         <span>최신 POST</span>
                     </div>
                     <div className="h-[530px] rounded-xl w-full">
-                        <div className="w-full h-3/4">
-                            <Image alt="post_img" src={latestPost.thumbnail} priority layout="responsive" width={100} height={100} />
+                        <div className="w-full h-3/4 relative">
+                            <Image alt="post_img" src={latestPost.thumbnail} priority fill={true} />
                         </div>
                         <div className="w-full h-1/4 flex flex-col justify-start">
                             <div className="h-1/2 line-clamp-1">
