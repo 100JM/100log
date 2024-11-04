93a44221fa1b8fffa0e5e4dc8051dbd88f9175d2
100JM <bjm_9310@naver.com>
2024-11-04 17:56:27 +0900

    update

--- app/components/PostList.tsx
+++ app/components/PostList.tsx
@@ -5,7 +5,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 const PostList: React.FC = async () => {
     const post = await getAllPosts();
 
-    // console.log(post);
+    console.log(post);
     return (
         <div className="w-[90%] grid gap-6 gap-y-10 mb-6 post-list">
             {Array.from({length: 6}, (_, index) => {
