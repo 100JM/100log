dd78d95779f391b1c8b2ac962512b4b1092cc022
100JM <bjm_9310@naver.com>
2024-11-05 17:54:58 +0900

    framer-motion 추가

--- app/components/PostList.tsx
+++ app/components/PostList.tsx
@@ -7,6 +7,7 @@ import Tags from "./Tags";
 import { useEffect } from "react";
 import usePost from "../store/usePost";
 import { Skeleton } from "@/components/ui/skeleton";
+import { AnimatePresence, motion } from 'framer-motion';
 
 const PostList: React.FC = () => {
     const { setTotalPages, currentPage, postList, setPostList } = usePost();
@@ -34,44 +35,69 @@ const PostList: React.FC = () => {
         }
     };
 
-    useEffect(() => {
+    const fadeVariants = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+        exit: { opacity: 0 }
+    };
+
+    const fadeTransitionSettings = {
+        duration: 1,
+        ease: "easeInOut"
+    };
+
+    useEffect(() => {                                                                                                                                    
         fetchGetAllPosts();
     }, [currentPage]);
 
     return (
-        <div className="w-[90%] grid gap-6 gap-y-10 mb-6 post-list">
+        <>
             {
                 postList.length > 0
-                ?
-                postList.map((post) => {
-                    return (
-                        <Link href={`posts/${post.slug}`} key={post.slug} className="h-[450px]">
-                            <div className="h-2/3 relative">
-                                <Image alt="post_img" src={post.thumbnail} fill className="rounded-2xl" />
-                            </div>
-                            <div className="h-1/3 mt-3">
-                                <div className="line-clamp-1 flex items-center">
-                                    <Tags tags={undefined} date={post.date} />
+                    ?
+                    <AnimatePresence>
+                        <motion.div
+                            key="post-list"
+                            variants={fadeVariants}
+                            initial="hidden"
+                            animate="visible"
+                            exit="exit"
+                            transition={fadeTransitionSettings}
+                            className="w-[90%] grid gap-6 gap-y-10 mb-6 post-list"
+                        >
+                            {postList.map((post) => {
+                                return (
+                                    <Link href={`posts/${post.slug}`} key={post.slug} className="h-[450px]">
+                                        <div className="h-2/3 relative">
+                                            <Image alt="post_img" src={post.thumbnail} fill className="rounded-2xl" />
+                                        </div>
+                                        <div className="h-1/3 mt-3">
+                                            <div className="line-clamp-1 flex items-center">
+                                                <Tags tags={undefined} date={post.date} />
+                                            </div>
+                                            <h2 className="text-3xl line-clamp-2 mt-3">{post.title}</h2>
+                                        </div>
+                                    </Link>
+                                );
+                            })}
+                        </motion.div>
+                    </AnimatePresence>
+                    :
+                    <div className="w-[90%] grid gap-6 gap-y-10 mb-6 post-list">
+                        {Array.from({ length: pagePerPost }, (_, index) => {
+                            return (
+                                <div key={index} className="h-[450px]">
+                                    <Skeleton className="h-2/3 rounded-2xl" />
+                                    <div className="h-1/3 mt-3">
+                                        <Skeleton className="h-11 w-36 rounded-full" />
+                                        <Skeleton className="h-11 mt-3 rounded-2xl" />
+                                    </div>
                                 </div>
-                                <h2 className="text-3xl line-clamp-2 mt-3">{post.title}</h2>
-                            </div>
-                        </Link>
-                    );
-                })
-                :
-                Array.from({ length: pagePerPost }, (_, index) => {
-                    return (
-                        <div key={index} className="h-[450px]">
-                            <Skeleton className="h-2/3 rounded-2xl" />
-                            <div className="h-1/3 mt-3">
-                                <Skeleton className="h-11 w-36 rounded-full" />
-                                <Skeleton className="h-11 mt-3 rounded-2xl" />
-                            </div>
-                        </div>
-                    );
-                })
+                            );
+                        })}
+                    </div>
             }
-        </div>
+        </>
     );
 };
 
