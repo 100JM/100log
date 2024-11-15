eae535a6a27c0c164fd38759677eb4f017d23157
100JM <bjm_9310@naver.com>
2024-11-15 10:20:16 +0900

    meta data 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,36 @@ const dohyeon = Do_Hyeon({
 
 export const metadata: Metadata = {
   title: "백종민 개발 블로그",
-  description: "성장하자",
+  description: "웹 개발자 백종민입니다.",
+  keywords: ['Web', 'Front-end', 'Javascript', 'blog', 'developer', '개발', '프론트앤드', '웹 개발자', '블로그'],
+  openGraph: {
+    title: '백종민 개발 블로그',
+    description: "웹 개발자 백종민입니다.",
+    images: [
+      {
+        url: 'https://100-log.vercel.app/images/openGraph_image.jpg',
+        alt: 'main_logo',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+    url: 'https://100-log.vercel.app/',
+    siteName: '100LOG'
+  },
+  twitter: {
+    title: '백종민 개발 블로그',
+    description: "웹 개발자 백종민입니다.",
+    images: [
+      {
+        url: 'https://100-log.vercel.app/images/openGraph_image.jpg',
+        alt: 'main_logo',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
