19cb8bcd505d837f0598a0e9fa3910ba4115a439
100JM <bjm_9310@naver.com>
2024-11-18 15:33:28 +0900

    날씨 정보

--- app/components/WeatherWidget.tsx
+++ app/components/WeatherWidget.tsx
@@ -2,8 +2,10 @@
 
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import { AnimatePresence, motion } from 'framer-motion';
+import { fadeTransitionSettings, fadeVariants } from "../utils/framer";
 
-interface regionInterface {
+interface RegionInterface {
     address_name: string;
     code: string;
     region_1depth_name: string;
@@ -15,37 +17,70 @@ interface regionInterface {
     y: number;
 }
 
+interface WeatherObjectInterface {
+    [key: string] : string;
+}
+
+const WEATHER_OBJECT: WeatherObjectInterface = {
+    Clear: 'ri-sun-line',
+    Clouds: 'ri-foggy-line',
+    Rain: 'ri-heavy-showers-line',
+    Snow: 'ri-snowflake-line',
+    Atmosphere: 'ri-mist-fill',
+    Thunderstorm: 'ri-flashlight-line'
+};
+
 const WeatherWidget: React.FC = () => {
     const [addr, setAddr] = useState<string>('');
+    const [weather, setWeather] = useState<{
+        main: string;
+        description: string;
+    }>({
+        main: '',
+        description: ''
+    });
     const weather_api_key = process.env.NEXT_PUBLIC_WEATHER_API;
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(async (geo) => {
             const { latitude, longitude } = geo.coords;
 
-            const kakaoGeoUrl = `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${longitude}&y=${latitude}`;
-
-            const response = await axios.get(kakaoGeoUrl, {
-                headers: {
-                    Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}`,
-                }
-            });
-
-            if (response.status === 200) {
-                const addressName = response.data.documents.find((i: regionInterface) => {
-                    return i.region_type === 'B'
+            try {
+                const kakaoGeoUrl = `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${longitude}&y=${latitude}`;
+                const response = await axios.get(kakaoGeoUrl, {
+                    headers: {
+                        Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}`,
+                    }
                 });
 
-                setAddr(addressName.address_name);
-            }
-            
-            if (weather_api_key) {
+                if (response.status === 200) {
+                    const addressName = response.data.documents.find((i: RegionInterface) => {
+                        return i.region_type === 'B'
+                    });
 
-                const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weather_api_key}&lang=kr`;
-    
-                const weatherResponse = await axios.get(openWeatherUrl);
-    
-                console.log(weatherResponse);
+                    setAddr(`${addressName.region_1depth_name} ${addressName.region_2depth_name}`);
+
+                    try {
+                        if (weather_api_key) {
+                            const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weather_api_key}&lang=kr`;
+                            const weatherResponse = await axios.get(openWeatherUrl);
+
+                            if (weatherResponse.status === 200) {
+                                setWeather((prev) => {
+                                    return {
+                                        ...prev,
+                                        main: weatherResponse.data.weather[0].main,
+                                        description: weatherResponse.data.weather[0].description,
+                                    }
+                                });
+                            }
+                        }
+                    } catch (error) {
+                        console.log('open weather error :', error);
+                    }
+                }
+            } catch (error) {
+                console.log('kakao geo error :', error);
             }
         });
 
@@ -53,10 +88,22 @@ const WeatherWidget: React.FC = () => {
     }, []);
 
     return (
-        <div>
-            <p className="text-xs xxs:text-sm">{addr}</p>
-            <i className="ri-sun-line"></i>
-        </div>
+        <AnimatePresence>
+            <motion.div
+                key="weather"
+                variants={fadeVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={fadeTransitionSettings}
+            >
+                <p className="text-xs xxs:text-sm">{addr}</p>
+                <p>
+                    <i className={`${WEATHER_OBJECT[weather?.main]} pr-2`}></i>
+                    <span>{weather?.description}</span>
+                </p>
+            </motion.div>
+        </AnimatePresence>
     );
 };
 
